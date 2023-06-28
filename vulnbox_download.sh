867a272d1961709b9ddea3ac06fd70a783604033
Edoardo Torrini <edoardo.torrini@gmail.com>
2023-06-28 17:16:41 +0200

    config for the competition

--- vulnbox_download.sh
+++ vulnbox_download.sh
@@ -3,7 +3,7 @@ set -e
 sshpass -V > /dev/null
 
 mkdir -p services
-sshpass -p ggm6vjma8poowlbxig0iwt0kqfayih0i \
+sshpass -p 50052179a37112671e53e9cd4a12c3ec16798d3948720a0c7c2f94124edb975b \
   rsync -r --exclude '/root/ctffarm' \
   --exclude '/root/packmate' \
   --exclude '/root/snap' \
