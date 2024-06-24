876d981e512d3f4edcf65135846232665e59e549
Giulio Barabino <giuliobarabino99@gmail.com>
2024-06-24 17:14:27 +0200

    improved script robustness (kinda)

--- gen_env.py
+++ gen_env.py
@@ -3,18 +3,38 @@
 import json
 import os
 import sys
+import ipaddress
 
 ENV_FILE_PATH = '.env.json'
 
 if os.path.isfile(ENV_FILE_PATH):
-    print(f'WARN: {ENV_FILE_PATH} already exists. Nothing done.', file=sys.stderr)
+    print(f'WARN: {ENV_FILE_PATH} already exists. Nothing done.',
+          file=sys.stderr)
     exit(1)
 
-vulnbox_ip = input('Enter the vulnbox IP: ')
+
+try:
+    vulnbox_ip = input('Enter the vulnbox IP: ')
+except ValueError:
+    print(f'ERROR: {vulnbox_ip} is not a valid IP address')
+    exit(1)
 gameserver_url = input('Enter gameserver URL for flag submission: ')
+if 'http://' not in gameserver_url:
+    print(f'ERROR: {gameserver_url} is not a valid url')
+    exit(1)
+if ':8080' not in gameserver_url:
+    print(f'ERROR: {gameserver_url} does not contain a valid port')
+    exit(1)
+if not gameserver_url.endswith('/flag'):
+    print(f'ERROR: {gameserver_url} does not contain a valid endpoint')
+    exit(1)
 team_token = input('Enter the team token: ')
 number_of_teams = int(input('Enter the number of teams: '))
-team_ip_format = input('Enter the Python format string for team IP: ')
+team_ip_format = "f'" + \
+    input('Enter the Python format string for team IP: ') + "'"
+if '{i}' not in team_ip_format:
+    print(f'ERROR: {team_ip_format} is not a valid format string.')
+    exit(1)
 game_interface = input('Enter network interface name for the game: ')
 
 env = {
@@ -33,3 +53,8 @@ with open(ENV_FILE_PATH, 'w') as f:
     json.dump(env, f, indent=4)
 
 os.chmod(ENV_FILE_PATH, 0o600)
+
+f = open('run_exploit.sh', 'r').readlines()
+f[2] = f'\t--server-pass {env['ctffarm_password']} \\\n'
+with open('run_exploit.sh', 'w') as file:
+    file.writelines(f)
